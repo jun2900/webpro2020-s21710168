4d65bf730edf89056facf3c97ff2fd124a49a115
jun2900 <jeremiahmaramis00@gmail.com>
2020-09-10 13:20:10 +0800

    "Pertemuan 7 if else dengan looping"

---
+++ pertemuan07/index.js
@@ -0,0 +1,14 @@
+//if else
+const ip = 3.9;
+if (ip >= 3.87) {
+    console.log("Suma Cumlaude");
+} else if (ip >= 3.67) {
+    console.log("Magna Cumlaude");
+} else if (ip >= 3.5) {
+    console.log("Cumlaude");
+}
+
+//Looping
+for (let i = 0; i < 5; i++) {
+    console.log("Kamu Hebat");
+}
