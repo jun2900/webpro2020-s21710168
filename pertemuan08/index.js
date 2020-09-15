049e66d3b3cd5dce8de292371b793507f7f0fd67
jun2900 <jeremiahmaramis00@gmail.com>
2020-09-15 14:50:42 +0800

    "Exercise 2 (Pertemuan08)"

---
+++ pertemuan08/index.js
@@ -0,0 +1,24 @@
+const account = {
+    name: "John",
+    expenses: []
+};
+
+function addExpenses(description, ammount) {
+    account.expenses.push({
+        description: description,
+        ammount: ammount
+    });
+}
+
+function getAccountSummary() {
+    let total = 0;
+    let totalExpense = account.expenses.forEach(expense => {
+        total += expense.ammount;
+    });
+    console.log(`Total pengeluaran ${account.name} adalah Rp.${total}`);
+}
+
+addExpenses("Beli Makanan", 15000);
+addExpenses("Ongkos", 30000);
+console.log(account.expenses);
+getAccountSummary();
