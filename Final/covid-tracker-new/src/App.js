2fd4c89ca1cd46a8f87bce84cc7cfcf9a1576821
jun2900 <jeremiahmaramis00@gmail.com>
2020-11-29 01:01:50 +0800

    Connected global api

--- Final/covid-tracker-new/src/App.js
+++ Final/covid-tracker-new/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { fetchData } from './api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        const fetchMyAPI = async () => {
+            const initialData = await fetchData()
+
+            setData(initialData)
+        }
+
+        fetchMyAPI()
+    }, [setData])
+
+    console.log(data)
+    return (
+        <div>
+            <p>Hello World</p>
+        </div>
+    )
 }
 
-export default App;
+export default App
